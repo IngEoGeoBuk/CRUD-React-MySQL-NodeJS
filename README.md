45b9a0f1bd01c5aca547d00ed0e72ca53bbc13cd
kurt <kurt.kang@factblock.com>
2024-12-22 14:54:12 +0900

    feat: update react18

--- README.md
+++ README.md
@@ -1,16 +1,22 @@
 # CRUD-React-MySQL-NodeJS
+
 This is Basic CRUD Project Using React, MySQL, NodeJS
 https://www.youtube.com/watch?v=re3OIOr9dJI&t=1s&ab_channel=PedroTech
 
-1. Setting your MySQL password.
--> change server > index.js // pawword: "put your MySQL Password"
+1. Setting .env
+   client
+
+- REACT_APP_API_URL=http://localhost:3001/api
+  server:
+- DB_DATABASE: your database
+- DB_PASSWORD: your password
 
 2. If there's nothing data on the MySQL, Setting your MySQL.
-(Error: ER_NOT_SUPPORTED_AUTH_MODE: Client does not support authentication protocol requested by server; consider upgrading MySQL client)
+   (Error: ER_NOT_SUPPORTED_AUTH_MODE: Client does not support authentication protocol requested by server; consider upgrading MySQL client)
 
-  2-1) Launch MySQL Installer
-  
-  2-2) Go to MySQL Server Reconfigure(Click Blue One.)
-  
-  2-3) In the 'Authentication Method' stage, check "Use Legacy Authentication Method (Retain MySQL 5.x Compatibility)". Second One!
+2-1) Launch MySQL Installer
+
+2-2) Go to MySQL Server Reconfigure(Click Blue One.)
+
+2-3) In the 'Authentication Method' stage, check "Use Legacy Authentication Method (Retain MySQL 5.x Compatibility)". Second One!
 (No First one "Use Strong Password Encryption for Authenti~~~ (RECOMMENDED)")
--- client/src/App.js
+++ client/src/App.js
@@ -1,72 +1,100 @@
-import React, { useState, useEffect } from 'react'
-import './App.css';
-import Axios from 'axios'
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Axios from "axios";
 
 function App() {
-
-  const [movieName, setMovieName] = useState('');
-  const [movieReview, setMovieReview] = useState('');
+  const [movieName, setMovieName] = useState("");
+  const [movieReview, setMovieReview] = useState("");
   const [movieReviewList, setMovieReviewList] = useState([]);
 
-  const [newMovieReview, setNewMovieReview] = useState('');
+  const [newMovieReview, setNewMovieReview] = useState("");
 
   // useEffect는 시작하자마자 하는 거라 생각하면 편함.
   useEffect(() => {
-    Axios.get("http://localhost:5000/api/get")
-    .then((response) => {
+    Axios.get(`${process.env.REACT_APP_API_URL}/get`).then((response) => {
       // console.log(response.data);
       setMovieReviewList(response.data);
-    })
-  }, [])
+    });
+  }, []);
 
   const submitReview = () => {
-    Axios.post("http://localhost:5000/api/insert",
-    {movieName: movieName, movieReview: movieReview});
-    
-    setMovieReviewList([...movieReviewList, {movieName: movieName, movieReview:movieReview}, 
+    Axios.post(`${process.env.REACT_APP_API_URL}/insert`, {
+      movieName: movieName,
+      movieReview: movieReview,
+    });
+
+    setMovieReviewList([
+      ...movieReviewList,
+      { movieName: movieName, movieReview: movieReview },
     ]);
     window.location.reload();
   };
 
   const deleteReview = (movieName) => {
-    Axios.delete(`http://localhost:5000/api/delete/${movieName}`);
+    Axios.delete(`${process.env.REACT_APP_API_URL}/delete/${movieName}`);
     window.location.reload();
-  }
+  };
 
   const updateReview = (movieName) => {
-    Axios.put(`http://localhost:5000/api/update`,
-    {movieName: movieName, movieReview: newMovieReview}
-    );
+    Axios.put(`${process.env.REACT_APP_API_URL}/update`, {
+      movieName: movieName,
+      movieReview: newMovieReview,
+    });
     setNewMovieReview("");
     window.location.reload();
-  }
+  };
 
   return (
     <div className="App">
       <h1>CRUD APPLICATION</h1>
       <div className="form">
         <label>Movie Name:</label>
-        <input type="text" name="movieName" onChange={(e) => {
-          setMovieName(e.target.value)
-        }} />
+        <input
+          type="text"
+          name="movieName"
+          onChange={(e) => {
+            setMovieName(e.target.value);
+          }}
+        />
         <label>Review:</label>
-        <input type="text" name="review" onChange={(e) => {
-          setMovieReview(e.target.value);
-        }} />
+        <input
+          type="text"
+          name="review"
+          onChange={(e) => {
+            setMovieReview(e.target.value);
+          }}
+        />
         <button onClick={submitReview}>Submit</button>
-        {movieReviewList.map((val) => {
-          return (
-            <div className="card">
-              <h1>{val.movieName}</h1>
-              <p>{val.movieReview}</p>
-              <button onClick={() => {deleteReview(val.movieName)}}>Delete</button>
-              <input type="text" id="updateInput" onChange={(e) => {
-                setNewMovieReview(e.target.value);
-              }}/>
-              <button onClick={() => {updateReview(val.movieName)}}>Update</button>
-            </div>
-          );
-        })} 
+        {movieReviewList &&
+          movieReviewList.map((val) => {
+            return (
+              <div className="card">
+                <h1>{val.movieName}</h1>
+                <p>{val.movieReview}</p>
+                <button
+                  onClick={() => {
+                    deleteReview(val.movieName);
+                  }}
+                >
+                  Delete
+                </button>
+                <input
+                  type="text"
+                  id="updateInput"
+                  onChange={(e) => {
+                    setNewMovieReview(e.target.value);
+                  }}
+                />
+                <button
+                  onClick={() => {
+                    updateReview(val.movieName);
+                  }}
+                >
+                  Update
+                </button>
+              </div>
+            );
+          })}
       </div>
     </div>
   );
